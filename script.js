db5a4bfe7599bb299bdb258b0dc81b1e63868ae7
sauromay <justineislearning@gmail.com>
2021-12-02 17:12:13 +0300

    Random Colors Grid

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 console.log("CONNECTED");
+
+var colors = [
+  "rgb(255, 0, 0)",
+  "rgb(0, 255, 0)",
+  "rgb(0, 0, 255)",
+  "rgb(255, 255, 0)",
+  "rgb(255, 0, 255)",
+  "rgb(0, 255, 255)",
+];
+
+var squares = document.querySelectorAll(".square");
+var pickedColor = colors[5];
+var colorDisplay = document.getElementById("colorDisplay");
+
+colorDisplay.textContent = pickedColor;
+
+for (let i = 0; i < squares.length; i++) {
+  //adding colors to the squares
+  squares[i].style.background = colors[i];
+  //adding event listeners to the squares
+}
